18fc0e9053c30a05ad39d3c5b064204b80f769a4
Oscar Eduardo Palomino Cardenas <oscar_epalomino@soy.sena.edu.co>
2025-10-16 15:28:52 -0500

    Update LogOperador.jsx

--- src/pages/LogOperador.jsx
+++ src/pages/LogOperador.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api/api.js';
-import NavbarOperador from '../Components/NavbarOperador';
+import NavbarOperador from '../components/NavbarOperador';
 
 const accionesOpciones = [
     'Todas las acciones',
