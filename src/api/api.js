f0d81caee4887be93c4b545fca86e18c0ece09fc
Oscar Eduardo Palomino Cardenas <oscar_epalomino@soy.sena.edu.co>
2025-10-16 15:38:22 -0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/api/';
+const BASE_URL = 'http://bancodemo_backend:8000/api/';
 
 // Crear instancia de Axios
 const api = axios.create({
