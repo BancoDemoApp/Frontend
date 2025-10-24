32f8beb6d6b577fef4e5f1e520ad044917df374c
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-10-24 14:42:06 -0500

    configurando para hostinger

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+
+const basename = import.meta.env.MODE === 'production' ? '/bancodemo' : '/'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+  <React.StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-      react(),
-  ],
-})
+// Detecta si está en modo producción usando las variables de Vite
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === 'production' ? '/bancodemo/' : '/',
+  server: {
+    host: '0.0.0.0', // accesible desde fuera del contenedor
+    port: 5173,
+    allowedHosts: [
+      'oscarpalomino.dev',
+      'www.oscarpalomino.dev'
+    ],
+  },
+}))
