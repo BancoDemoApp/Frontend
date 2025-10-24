da05d73d3296ba4bcbe3e8d0b859844c95abac4f
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-10-24 15:56:05 -0500

    configurando para hostinger

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = (process.env.VITE_ENV || 'development') === 'production'
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_ENV === 'production' ? '/bancodemo/' : '/',
+  base: isProduction ? '/bancodemo/' : '/',
   server: {
     host: '0.0.0.0',
     port: 5173,
