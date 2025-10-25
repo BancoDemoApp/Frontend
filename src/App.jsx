22c7ec9676091b666ce58c574a6aea0c82f78947
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-10-24 22:58:43 -0500

    configurando para hostinger

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -18,108 +18,106 @@ import Retirar from "./pages/Retirar";
 import LogOperador from './pages/LogOperador';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                {/* Rutas públicas */}
-                <Route element={<PublicLayout />}>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                </Route>
+  return (
+    <Routes>
+      {/* Rutas públicas */}
+      <Route element={<PublicLayout />}>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
 
-                {/* Cliente */}
-                <Route element={<ClienteLayout />}>
-                    <Route
-                        path="/dashboard"
-                        element={
-                            <PrivateRoute tipoPermitido="Cliente">
-                                <DashboardCliente />
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="/transferencia"
-                        element={
-                            <PrivateRoute tipoPermitido="Cliente">
-                                <TransferenciaCliente />
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="/historial"
-                        element={
-                            <PrivateRoute tipoPermitido="Cliente">
-                                <HistorialClientes />
-                            </PrivateRoute>
-                        }
-                    />
-                </Route>
+      {/* Cliente */}
+      <Route element={<ClienteLayout />}>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute tipoPermitido="Cliente">
+              <DashboardCliente />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/transferencia"
+          element={
+            <PrivateRoute tipoPermitido="Cliente">
+              <TransferenciaCliente />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/historial"
+          element={
+            <PrivateRoute tipoPermitido="Cliente">
+              <HistorialClientes />
+            </PrivateRoute>
+          }
+        />
+      </Route>
 
-                {/* Operador */}
-                <Route path="/operador" element={<OperadorLayout />}>
-                    <Route
-                        index
-                        element={
-                            <PrivateRoute tipoPermitido="Operador">
-                                <DashboardOperador />
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="depositar"
-                        element={
-                        <PrivateRoute tipoPermitido="Operador">
-                            <Depositar />
-                        </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="retirar"
-                        element={
-                            <PrivateRoute tipoPermitido="Operador">
-                                <Retirar />
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="logs"
-                        element={
-                            <PrivateRoute tipoPermitido="Operador">
-                                <LogOperador />
-                            </PrivateRoute>
-                        }
-                    />
-                </Route>
+      {/* Operador */}
+      <Route path="/operador" element={<OperadorLayout />}>
+        <Route
+          index
+          element={
+            <PrivateRoute tipoPermitido="Operador">
+              <DashboardOperador />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="depositar"
+          element={
+            <PrivateRoute tipoPermitido="Operador">
+              <Depositar />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="retirar"
+          element={
+            <PrivateRoute tipoPermitido="Operador">
+              <Retirar />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="logs"
+          element={
+            <PrivateRoute tipoPermitido="Operador">
+              <LogOperador />
+            </PrivateRoute>
+          }
+        />
+      </Route>
 
-                {/* Rutas compartidas (Cliente + Operador) */}
-                <Route
-                    path="/perfil"
-                    element={
-                        <PrivateRoute tipoPermitido={['Cliente', 'Operador']}>
-                            <Perfil />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/perfil/editar"
-                    element={
-                        <PrivateRoute tipoPermitido={['Cliente', 'Operador']}>
-                            <EditarPerfil />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/perfil/cambiar-contrasena"
-                    element={
-                        <PrivateRoute tipoPermitido={['Cliente', 'Operador']}>
-                            <CambiarContrasena />
-                        </PrivateRoute>
-                    }
-                />
-            </Routes>
-        </Router>
-    );
+      {/* Rutas compartidas */}
+      <Route
+        path="/perfil"
+        element={
+          <PrivateRoute tipoPermitido={['Cliente', 'Operador']}>
+            <Perfil />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/perfil/editar"
+        element={
+          <PrivateRoute tipoPermitido={['Cliente', 'Operador']}>
+            <EditarPerfil />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/perfil/cambiar-contrasena"
+        element={
+          <PrivateRoute tipoPermitido={['Cliente', 'Operador']}>
+            <CambiarContrasena />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
